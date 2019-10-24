623f4ab695d57b0ccb573c9c90ca7ab2fab0cdbb
Bobby Wertman <robert.wertman@gmail.com>
2019-10-23 21:30:03 -0400

    Allow TemplateEngines to throw IOException

--- generator/src/main/java/com/casualsuperman/portent/TemplateEngine.java
+++ generator/src/main/java/com/casualsuperman/portent/TemplateEngine.java
@@ -1,8 +1,9 @@
 package com.casualsuperman.portent;
 
+import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
 
 public interface TemplateEngine {
-	void writeTo(String templateName, Reader reader, Context context, Writer writer);
+	void writeTo(String templateName, Reader reader, Context context, Writer writer) throws IOException;
 }
